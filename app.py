82fcb7b5b88097231cb5155dbc8d5bdbdc614838
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2023-12-06 23:39:13 +0530

    working version

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from core.extract_skills_education import extract_education_and_skills
 
 app = Flask(__name__)
 
-# Define the folder for raw resumes
+# folder for raw resumes
 UPLOAD_FOLDER = 'raw_resumes'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -30,25 +30,25 @@ def process_resume(resume_path, jd_content):
         for page_num in range(pdf_document.page_count):
             page = pdf_document[page_num]
             text += page.get_text()
-    # Extract candidate information manually
+    # Extracting candidate information manually
     candidate_name, candidate_email = extract_candidate_info(text)
 
-    # Extract education and skills information manually
+    # Extracting education and skills information manually
     education_text, skills_text = extract_education_and_skills(text)
 
      
 
     
-    # Simple matching algorithm: count the occurrences of JD words in the resume
+    # matching algorithm: count the occurrences of JD words in the resume
     jd_words = set(word.lower() for word in jd_content.split())
     resume_words = text.lower().split()
     match_count = sum(1 for word in resume_words if word in jd_words)
 
-    # Adjust the threshold based on JD length
+    # Adjusting the basic threshold 
     jd_length = len(jd_words)
     threshold = min(jd_length // 5, 10)
 
-    # Assign a star rating based on the match count
+    # Assigning a  rating based on the match count
     star_rating = min(match_count // threshold, 5) + 1
     
     
@@ -60,7 +60,7 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Get uploaded JD and resumes
+    # Getting uploaded JD and resumes
     jd_file = request.files['jd']
     resume_files = request.files.getlist('resumes[]')
 
@@ -71,7 +71,7 @@ def upload():
     # Process JD
     jd_content = process_jd(jd_path)
 
-    # Process and analyze each resume
+    # Processing and analyze each resume
     results = []
     for resume_file in resume_files:
         resume_path = os.path.join(app.config['UPLOAD_FOLDER'], resume_file.filename)
